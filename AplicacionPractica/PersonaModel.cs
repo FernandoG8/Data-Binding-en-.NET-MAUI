b39176bdf9df44af696210036678e18f1d3000ab
FernandoG8 <al064504@uacam.mx>
2024-11-13 20:41:38 -0600

    feat: :bear: incio de la aplicación

---
+++ AplicacionPractica/PersonaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPractica
+{
+    public class PersonaModel
+    {
+        public int id { get; set; }
+        public string nombre {  get; set; }
+        public string apellido { get; set; }
+        public string sexo { get; set; }
+        public string fh_nac { get; set; }
+        public int id_rol {  get; set; }
+    }
+}
---
+++ AplicacionPractica/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AplicacionPractica
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
